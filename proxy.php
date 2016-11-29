c50ef1c6cb62308e26866bc6edcf604e531608ca
gt000 <gtanzi@gmail.com>
2016-11-29 08:56:07 +0100

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -27,10 +27,9 @@ stream_context_set_default(
 
 
 /* 1. Check the URL to retrieve */
-if( FALSE == isset($_GET["dst"]) ) {
-	error("GET parameter dst is not set.");
-}
-$url = $_GET["dst"];
+
+$url = getenv("URL");
+$url = "http://" . $url;
 
 
 /* 2. Add http:// to URL */
@@ -39,50 +38,6 @@ if(strpos($url, "http://") === 0 || strpos($url, "https://") === 0) {
 	$url = "http://" . $url;
 }
 
-
-/* 3. Check the timestamp */
-$headers = get_headers($url, 1);
-$ts_remote = null;
-if($headers) {
-	if( array_key_exists('Last-Modified', $headers) ) {
-		// most of web sites
-		$ts_remote = strtotime($headers['Last-Modified']);
-	} else if( array_key_exists('etag', $headers) ) {
-		// dl.dropboxusercontent.com
-		$ts_remote = $headers['etag'];
-	} else if( array_key_exists('ETag', $headers) ) {
-		$ts_remote = $headers['ETag'];
-	}
-}
-header("X-Timestamp-Remote: $ts_remote");
-
-if( array_key_exists('Content-Type', $headers) ) {
-	header("Content-Type: " . $headers['Content-Type']);
-}
-
-
-/* 4. Init the cache */
-$options = array(
-	'lifeTime' => null,
-	'pearErrorMode' => CACHE_LITE_ERROR_DIE
-);
-$cache = new Cache_Lite($options);
-
-
-/* 5. Check if cached or not */
-if($ts_remote != null) {
-	$ts_local = $cache->get(timestamp_url_key($url));
-	if($ts_local) {
-		header("X-Timestamp-Local: $ts_local");
-		if($ts_remote == $ts_local) {
-			header("X-Cache-Used: Yes");
-			echo $cache->get($url);
-			exit;
-		}
-	}
-}
-
-
 /* 6. Retrieve the remote data */
 $ret = file_get_contents($url, FALSE);
 if( FALSE === $ret ) {
@@ -90,16 +45,6 @@ if( FALSE === $ret ) {
 }
 
 
-/* 7. Cache */
-if( $ts_remote ) {
-	header("X-Cache-Updated: Yes");
-	$cache->save($ret, $url);
-	$cache->save($ts_remote, timestamp_url_key($url));
-}
-
-
-/* 8. Show */
-header("X-Cache-Used: No");
 echo $ret;
 
 ?>
